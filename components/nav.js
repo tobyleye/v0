5e9b4669e3f9a460a06862ec522485d2989a27fc
Oluwatobi <krisella74@gmail.com>
2025-05-07 13:03:44 +0100

    update long time portfolio

--- components/nav.js
+++ components/nav.js
@@ -1,38 +1,37 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-
 export function Nav() {
   return (
     <nav className="nav">
       <NavLink href="/">
-        {`<`}tobi{`/>`}
+        {`<`}Tobi{`/>`}
       </NavLink>
       <NavLink href="/projects">work</NavLink>
       <NavLink href="/about">personal</NavLink>
       <style jsx>{`
-          .nav {
-            display: flex;
-            margin-bottom: 50px;
-            gap: 20px;
-          }
+        .nav {
+          display: flex;
+          margin-bottom: 50px;
+          gap: 20px;
+        }
       `}</style>
     </nav>
-  )
+  );
 }
 
 function NavLink({ href, children }) {
-    const router = useRouter();
-    const path = router.asPath
-    const active = href === '/'  ? href === path : path.includes(href)
+  const router = useRouter();
+  const path = router.asPath;
+  const active = href === "/" ? href === path : path.includes(href);
 
-    return (
-      <div>
-        <Link href={href} className={`nav-link ${active ? "active" : ""}`}>
-          {children}
-        </Link>
-  
-        <style jsx global>{`
+  return (
+    <div>
+      <Link href={href} className={`nav-link ${active ? "active" : ""}`}>
+        {children}
+      </Link>
+
+      <style jsx global>{`
           .nav-link {
             position: relative;
             transition: 0.24s ease;
@@ -58,8 +57,6 @@ function NavLink({ href, children }) {
             width: 100%;
           }
         `}</style>
-      </div>
-    );
-  }
-  
-
+    </div>
+  );
+}
--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,19 @@
 export default function AboutPage() {
   return (
     <div className="about">
-      <p>A simple guy, and a real crowd pleaser.</p>
+      <p>A really simple guy who loves to build amazing products.</p>
       <p>
-        Besides building amazing products I love exploring new things and
+        Besides building amazing products, I enjoy exploring new things and
         places, listening to music, maths, working out, and teaching.
       </p>
+      <div className="fav-quote">
+        <p> My favourite quote is</p>
+        <p className="quote">
+          Great things are done by a series of small things brought together.
+        </p>
+      </div>
       <p>
-        {`
-        My favourite quote is "Great things are done by a series of small things
-        brought together."
-        `}
-      </p>
-      <p>
-        <a href="https://vs.co/t4mrhelh" target="_blank" rel="noreferrer">
+        <a href="https://vs.co/qjqnh58v" target="_blank" rel="noreferrer">
           Here
         </a>{" "}
         is a picture of me in the gym.
@@ -29,6 +29,27 @@ export default function AboutPage() {
           color: yellow;
           text-decoration: underline;
         }
+
+        .fav-quote {
+          background: rgba(255, 255, 255, 0.1);
+          border-radius: 5px;
+          padding: 20px;
+          margin-bottom: 20px;
+        }
+
+        .quote {
+          position: relative;
+          /* padding-left: 27px; */
+          font-style: italic;
+        }
+        /* .quote::before {
+          content: '"';
+          display: block;
+          font-size: 55px;
+          position: absolute;
+          left: 0;
+          top: -25px;
+        } */
       `}</style>
     </div>
   );
